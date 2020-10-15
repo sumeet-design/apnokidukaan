041b1bc429a6c52d619c99899024da69bc118032
vtf05 <56218201+vtf05@users.noreply.github.com>
2020-10-15 14:01:39 +0530

    Update models.py
    Added a user model in Blog

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,25 @@
-from django.db import models
+
 
 # Create your models here.
+from django.db import models
+from django.contrib.auth.models import AbstractUser
+from django.utils.translation import ugettext_lazy as _
+from django.conf import settings
+
+class User(AbstractUser):
+    username = models.CharField(max_length=50,blank=True, null=True)
+    email = models.EmailField(_('email address'), unique=True)
+
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = ['username', 'first_name', 'last_name']
+
+    def __str__(self):
+        return "{}".format(self.email)
+        
+        
+class UserProfile(models.Model):
+    user = models.OneToOneField(settings.AUTH_USER_MODEL, on_delete=models.CASCADE, related_name='profile')
+    native_name = models.CharField(max_length=5)
+    phone_no=models.CharField(max_length=10)
+    dob = models.DateField()
+    address = models.CharField(max_length=255)
